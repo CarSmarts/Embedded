1748e299414ad93ebc57c108bb6a12799e22718b
Jackson Smith <rjsmith1999@gmail.com>
2018-06-20 17:33:59 -0400

    Stuff

--- CarSmartsGateway/CAN.h
+++ CarSmartsGateway/CAN.h
@@ -1,15 +1,51 @@
 
-#define CAN0_INT 2                              // Set INT to pin 2
+const int SPI_CS_PIN = 10;
 
-void setupCAN(MCP_CAN CAN0) {
-      // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
-  if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK)
-    Serial.println("MCP2515 Initialized Successfully!");
-  else
-    Serial.println("Error Initializing MCP2515...");
-  
-  CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
 
-  pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
+unsigned char flagRecv = 0;
+unsigned char len = 0;
+unsigned char buf[8];
+char str[20];
+
+void MCP2515_ISR()
+{
+    flagRecv = 1;
 }
 
+void setupCAN()
+{
+    while (CAN_OK != CAN.begin(CAN_33KBPS))
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println(" Init CAN BUS Shield again");
+        delay(100);
+    }
+    Serial.println("CAN BUS Shield init ok!");
+
+    attachInterrupt(0, MCP2515_ISR, FALLING); // start interrupt
+}
+
+void proccessMessages() {
+    if(flagRecv) 
+    {                                   // check if get data
+        flagRecv = 0;                   // clear flag
+
+        // iterate over all pending messages
+        // If either the bus is saturated or the MCU is busy,
+        // both RX buffers may be in use and reading a single
+        // message does not clear the IRQ conditon.
+        while (CAN_MSGAVAIL == CAN.checkReceive()) 
+        {
+            // read data,  len: data length, buf: data buf
+            CAN.readMsgBuf(&len, buf);
+
+            // print the data
+            for(int i = 0; i<len; i++)
+            {
+                Serial.print(buf[i]);Serial.print("\t");
+            }
+            Serial.println();
+        }
+    }
+}
--- CarSmartsGateway/CarSmartsGateway.ino
+++ CarSmartsGateway/CarSmartsGateway.ino
@@ -1,6 +1,3 @@
-#include <mcp_can.h>
-#include <mcp_can_dfs.h>
-
 #include "Types.h"
 #include "SmartLockService.h"
 #include "LockController.h"
@@ -11,27 +8,26 @@
 #define UNLOCKED P0_23 //7 // unlocked sensor
 
 SmartLockService *smartLockServicePtr;
+UAR *smartLockServicePtr;
 
 LockController lockController(UNLOCKED, LOCKED, LOCK, UNLOCK);
 
 #include "BLEInit.h"
 #include "CAN.h"
 
-long unsigned int rxId;
-unsigned char len = 0;
-unsigned char rxBuf[8];
-char msgString[128];                        // Array to store serial string
-
-MCP_CAN CAN0(10);                               // Set CS to pin 10
+//long unsigned int rxId;
+//unsigned char len = 0;
+//unsigned char rxBuf[8];
+//char msgString[128];                        // Array to store serial string
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("Program Start");
+
+  setupCAN();
 
   BLE &ble = BLE::Instance();
   ble.init(setupBLE);
-
-  setupCAN(CAN0);
 
   /* SpinWait for initialization to complete. This is necessary because the
      BLE object is used in the main loop below. */
@@ -41,5 +37,6 @@ void setup() {
 }
 
 void loop() {
+  proccessMessages();
   BLE::Instance().waitForEvent();
 }
